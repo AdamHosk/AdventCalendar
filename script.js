519d3cb11bd124701134a4523eb1efd226cf5585
AdamHosk <hoskinsadam98@hotmail.com>
2022-11-03 13:43:16 +0000

    4

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 
 
-for (let index = 1; index < 26; index++) {
-    console.log(`
-        <div class="grid-item grid-item-${index}" id="window${index}">
-        <h1>${index}</h1>
-        </div>
+// for (let index = 1; index < 26; index++) {
+//     console.log(`
+//         <div class="grid-item grid-item-${index}" id="window${index}">
+//         <h1>${index}</h1>
+//         </div>
 
 
-    `);
+//     `);
     
+// }
+
+window1.addEventListener("click", logstuff);
+
+function logstuff(){
+    console.log("hello world");
 }
